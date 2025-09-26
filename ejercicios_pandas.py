44f14a007232ad2725a89979bca5ee94603d6a32
Jair171676 <143846363+Jair171676@users.noreply.github.com>
2025-09-25 19:18:32 -0600

    Update ejercicios_pandas.py

--- ejercicios_pandas.py
+++ ejercicios_pandas.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import doctest
 
+def crear_serie(lista):
+    crear_serie([1, 2, 3]).tolist()
+    [1, 2, 3]
+    return pd.Series(lista)
+
+def crear_dataframe(diccionario):
+    df = crear_dataframe({'a': [1, 2], 'b': [3, 4]})
+    df.shape
+    (2, 2)
+    list(df.columns)
+    ['a', 'b']
