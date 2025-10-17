636fad004735b5ba1e890e5fb66a74b380376cd8
Nehflime <173357@upslp.edu.mx>
2025-10-17 09:17:05 -0600

    Update ejercicios_pandas.py

--- ejercicios_pandas.py
+++ ejercicios_pandas.py
@@ -1,1 +1,195 @@
+import pandas as pd
+import doctest
 
+def crear_serie(lista):
+    """
+    Crea una Serie de Pandas a partir de una lista.
+    >>> crear_serie([1, 2, 3]).tolist()
+    [1, 2, 3]
+    """
+
+
+    return pd.Series(lista)
+
+serie_numeros = crear_serie([1,2,3])
+print(serie_numeros.tolist())
+
+def crear_dataframe(diccionario):
+    """
+    Crea un DataFrame a partir de un diccionario.
+    >>> df = crear_dataframe({'a': [1, 2], 'b': [3, 4]})
+    >>> df.shape
+    (2, 2)
+    >>> list(df.columns)
+    ['a', 'b']
+    """
+    df = pd.DataFrame(diccionario)
+    return df
+diccionario = {'a': [1, 2], 'b': [3, 4]}
+df = crear_dataframe(diccionario)
+print(df)
+
+def seleccionar_columna(df, columna):
+    """
+    Selecciona una columna de un DataFrame.
+    >>> df = pd.DataFrame({'x': [10, 20], 'y': [30, 40]})
+    >>> seleccionar_columna(df, 'x').tolist()
+    [10, 20]
+    """
+
+    return df[columna]
+
+df = pd.DataFrame({'x': [10, 20], 'y': [30, 40]})
+columna_seleccionada = seleccionar_columna(df, 'y')
+print(columna_seleccionada.tolist())
+
+def filtrar_por_condicion(df, columna, valor):
+    """
+    Filtra filas donde la columna es mayor que el valor dado.
+    >>> df = pd.DataFrame({'edad': [15, 25, 35]})
+    >>> filtrar_por_condicion(df, 'edad', 20)['edad'].tolist()
+    [25, 35]
+    """
+
+
+    return df[df[columna] > valor]
+
+df = pd.DataFrame({'edad': [15, 25, 35]})
+resultado = filtrar_por_condicion(df, 'edad', 20)
+print(resultado)
+
+def agregar_columna(df, nueva_columna, valores):
+    """
+    Agrega una nueva columna al DataFrame.
+    >>> df = pd.DataFrame({'a': [1, 2]})
+    >>> agregar_columna(df, 'b', [3, 4])['b'].tolist()
+    [3, 4]
+    """
+
+    df[nueva_columna] = valores
+    return df
+
+df = pd.DataFrame({'a': [1, 2]})
+agregar_columna(df, 'b', [3, 4])
+
+def crear_indexacion_jerarquica():
+    """
+    Crea una Serie con indexación jerárquica.
+    >>> s = crear_indexacion_jerarquica()
+    >>> s.loc['a', 1]
+    np.int64(1)
+    """
+    serie = pd.Series(range(1, 5), index=[['a', 'a', 'b', 'b'], [1, 2, 1, 2]])
+
+    return serie
+
+s=crear_indexacion_jerarquica()
+s.loc['a', 1]
+
+def suma_columnas(df, col1, col2):
+    """
+    Suma dos columnas de un DataFrame.
+    >>> df = pd.DataFrame({'x': [1, 2], 'y': [3, 4]})
+    >>> suma_columnas(df, 'x', 'y').tolist()
+    [4, 6]
+    """
+
+    df[col1 + col2] = df[col1] + df[col2]
+    return df[col1 + col2]
+
+df = pd.DataFrame({'x': [1, 2], 'y': [3, 4]})
+suma_columnas(df, 'x', 'y')
+
+def rellenar_nulos(df, valor):
+    """
+    Rellena valores nulos con un valor dado.
+    >>> df = pd.DataFrame({'a': [1, None, 3]})
+    >>> rellenar_nulos(df, 0)['a'].tolist()
+    [1.0, 0.0, 3.0]
+    """
+    df.fillna(valor, inplace=True)
+    return df
+
+df = pd.DataFrame({'a': [1, None, 3]})
+rellenar_nulos(df, 0)
+
+def agrupar_y_promediar(df, columna_agrupacion, columna_valores):
+    """
+    Agrupa por una columna y calcula el promedio de otra.
+    >>> df = pd.DataFrame({'grupo': ['A', 'A', 'B', 'B'], 'valor': [10, 20, 30, 40]})
+    >>> resultado = agrupar_y_promediar(df, 'grupo', 'valor')
+    >>> resultado['A'] == 15 and resultado['B'] == 35
+    True
+    """
+    df_agrupado = df.groupby(columna_agrupacion)[columna_valores].mean()
+    return df_agrupado
+
+df = pd.DataFrame({'grupo': ['A', 'A', 'B', 'B'], 'valor': [10, 20, 30, 40]})
+resultado = agrupar_y_promediar(df, 'grupo', 'valor')
+print(resultado)
+
+def ordenar_dataframe(df, columna):
+    """
+    Ordena un DataFrame por una columna.
+    >>> df = pd.DataFrame({'a': [3, 1, 2]})
+    >>> ordenar_dataframe(df, 'a')['a'].tolist()
+    [1, 2, 3]
+    """
+    df_ordenado = df.sort_values(by=columna)
+    return df_ordenado
+
+df = pd.DataFrame({'a': [3, 1, 2]})
+ordenar_dataframe(df, 'a')
+
+def combinar_dataframes(df1, df2, clave):
+    """
+    Combina dos DataFrames usando merge por una clave.
+    >>> df1 = pd.DataFrame({'id': [1, 2], 'valor': ['A', 'B']})
+    >>> df2 = pd.DataFrame({'id': [1, 2], 'extra': ['X', 'Y']})
+    >>> combinado = combinar_dataframes(df1, df2, 'id')
+    >>> combinado.shape
+    (2, 3)
+    """
+    df_combinado = pd.merge(df1, df2, on=clave)
+    return df_combinado
+
+df1 = pd.DataFrame({'id': [1, 2], 'valor': ['A', 'B']})
+df2 = pd.DataFrame({'id': [1, 2], 'extra': ['X', 'Y']})
+combinado = combinar_dataframes(df1, df2, 'id')
+print(combinado)
+
+def concatenar_dataframes(lista_df):
+    """
+    Concatena una lista de DataFrames.
+    >>> df1 = pd.DataFrame({'a': [1]})
+    >>> df2 = pd.DataFrame({'a': [2]})
+    >>> concatenado = concatenar_dataframes([df1, df2])
+    >>> concatenado['a'].tolist()
+    [1, 2]
+    """
+    df_concatenado = pd.concat(lista_df)
+    return df_concatenado
+
+
+df1 = pd.DataFrame({'a': [1]})
+df2 = pd.DataFrame({'a': [2]})
+concatenado = concatenar_dataframes([df1, df2])
+print(concatenado)
+
+def transformar_tipo(df, columna, tipo):
+    """
+    Transforma el tipo de datos de una columna.
+    >>> df = pd.DataFrame({'a': ['1', '2']})
+    >>> transformar_tipo(df, 'a', int)['a'].tolist()
+    [1, 2]
+    """
+    df[columna] = df[columna].astype(tipo)
+    return df
+
+df = pd.DataFrame({'a': ['1', '2']})
+transformar_tipo(df, 'a', int)
+
+# Ejecutar pruebas doctest
+import doctest
+import __main__
+doctest.testmod(__main__, verbose=True)
